87e9565c986c34612a112d2017d7cedc39ca5755
kt999 <kiteak9275@gmail.com>
2020-03-29 16:39:08 +0900

    1일차 공부

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+
+function Test({id,name, age, fav}) {
+    return (
+        <div className="test">
+            <div>id : {id}</div>
+            <div>name : {name}</div>
+            <div>age : {age}</div>
+            <div>fav : {fav[0]},{fav[1]},{fav[2]}</div>
+        </div>
+    );
+}
+
+Test.propTypes = {
+    id : PropTypes.number.isRequired,
+    name : PropTypes.string.isRequired,
+    age : PropTypes.number.isRequired,
+    fav : PropTypes.arrayOf(PropTypes.string.isRequired)
+}
+
+
+
+const peopleData = [
+    {
+        "id" : 1,
+        "name" : "kiteak",
+        "age" : 21,
+        "fav" : ["a","b","c"]
+    },
+    {
+        "id" : 2,
+        "name" : "json",
+        "age" : 35,
+        "fav" : ["d","e","f"]
+    }
+]
+
+function renderData(dataObject) {
+    return <Test key = {dataObject.id} id = {dataObject.id} name={dataObject.name}  age={dataObject.age}  fav={dataObject.fav} />
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        hello~~!!
+        {peopleData.map(renderData)}
     </div>
   );
 }
